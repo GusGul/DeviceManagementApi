7515857568830aabf203fc806db8f78a731722f3
GusSalgado <gustavo.salgado@farmarcas.com.br>
2024-12-06 14:47:21 -0300

    feat: initialize device service on application layer

---
+++ DeviceManagementApplication/Services/DeviceService.cs
@@ -0,0 +1,20 @@
+﻿using DeviceManagementDomain.Entities;
+using DeviceManagementDomain.Interfaces.Repositories;
+
+namespace Application.Services
+{
+    public class DeviceService
+    {
+        private readonly IDeviceRepository _repository;
+
+        public DeviceService(IDeviceRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> AddDevice(Device device)
+        {
+            return await _repository.AddDeviceAsync(device);
+        }
+    }
+}
