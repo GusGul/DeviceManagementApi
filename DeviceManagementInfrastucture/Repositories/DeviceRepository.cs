b2c876a6b257634f7290c27f97c0c225c666d54d
GusSalgado <gustavo.salgado@farmarcas.com.br>
2024-12-06 14:48:10 -0300

    feat: initialize device repository with npgsql

---
+++ DeviceManagementInfrastucture/Repositories/DeviceRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using DeviceManagementDomain.Entities;
+using DeviceManagementDomain.Interfaces.Repositories;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Data;
+
+namespace DeviceManagementInfrastucture.Repositories
+{
+    public class DeviceRepository : IDeviceRepository
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public DeviceRepository(IConfiguration configuration)
+        {
+            _dbConnection = new NpgsqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public async Task<int> AddDeviceAsync(Device device)
+        {
+            const string query = "INSERT INTO Devices (Name, Brand) VALUES (@Name, @Brand) RETURNING Id;";
+            return await _dbConnection.ExecuteScalarAsync<int>(query, device);
+        }
+
+        public Task DeleteDeviceAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Device?> GetDeviceAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Device>> GetDevicesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Device>> SearchDevicesByBrandAsync(string brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateDeviceAsync(Device device)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
