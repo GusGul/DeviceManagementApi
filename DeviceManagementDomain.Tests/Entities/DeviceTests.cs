d7ff49a6d7c0ef7ab37a7b2a9a6f4039df102c87
Gustavo Salgado Lima <gustavosalgadolima@gmail.com>
2024-12-07 00:31:30 -0300

    test: create device entity tests

--- DeviceManagementDomain.Tests/Entities/DeviceTests.cs
+++ DeviceManagementDomain.Tests/Entities/DeviceTests.cs
@@ -5,10 +5,8 @@ public class DeviceTests
     [Fact]
     public void Device_ShouldInitializeCorrectly()
     {
-        // Arrange & Act
         var device = new Device { Id = 1, Name = "Test Device", Brand = "Test Brand" };
 
-        // Assert
         Assert.Equal(1, device.Id);
         Assert.Equal("Test Device", device.Name);
         Assert.Equal("Test Brand", device.Brand);
